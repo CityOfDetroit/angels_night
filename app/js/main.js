315d2051df576b57775b58d116ad66317cf24446
J.Edgar Montes <montesje@ds.detroitmi.gov>
2017-10-31 11:37:44 -0400

    fix map click bug

--- app/js/main.js
+++ app/js/main.js
@@ -45,9 +45,12 @@ import mapboxgl from 'mapbox-gl';
       });
       // console.log(e.point);
       if (features.length) {
+        let date = new Date();
+        date = date.getDate();
+        (date === 30) ? date = 'volunteers30' : date = 'volunteers31';
         new mapboxgl.Popup()
             .setLngLat(features[0].geometry.coordinates[0][0])
-            .setHTML('<h2>Volunteers: ' + controller.scoutVolunteers[features[0].properties.area]['volunteers30'] + '</h2>')
+            .setHTML('<h2>Volunteers: ' + controller.scoutVolunteers[features[0].properties.area][date] + '</h2>')
             .addTo(this);
       }
     }
